edd07230c7152a75f63e62e4b373a9d4aca46f04
갈미현 <kalmihyeon@galmihyeon-ui-MacBookAir.local>
2023-10-04 14:10:06 +0900

    first commit with react-hook-form, react-query

--- src/App.js
+++ src/App.js
@@ -1,8 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { Controller, useForm } from 'react-hook-form';
+
+import {yupResolver} from "@hookform/resolvers/yup"
+
+import * as yup from "yup"
+
+
+const schema = yup.object({
+  firstName : yup.string().required(),
+  age: yup.number().positive().integer().required()
+}).required();
 
 function App() {
+
+  const fetchTodos = async({pageParams = 1}) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${pageParams}`)
+
+    if (res.ok) {
+      const result = await res.json();
+      return result;
+    }
+  }
+
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery({
+    queryKey: ['projects'],
+    queryFn: fetchTodos,
+    getNextPageParam: (lastPage, pages) =>{
+      // console.log('lastpage', lastPage);
+
+      return lastPage.id + 1;
+    },
+  })
+
+  console.log(process.env.REACT_APP_P_VALUE)
+
+  const {register, handleSubmit, watch, control, formState: {errors}} = useForm({
+    resolver: yupResolver(schema)
+  });
+
+  const onSubmit = (data) => console.log(data);
+
+  console.log(errors);
+
   return (
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -17,6 +68,16 @@ function App() {
         >
           Learn React
         </a>
+      <button onClick={() => fetchNextPage()}>Click</button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("firstName")} />
+      <p>{errors.firstName?.message}</p>
+        
+      <input {...register("age")} />
+      <p>{errors.age?.message}</p>
+      <input type='submit'/>
+      </form>
+      <p>{process.env.REACT_APP_P_VALUE}</p>
       </header>
     </div>
   );
