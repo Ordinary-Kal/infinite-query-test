5c906c0dbf49b3d201c64b3b0a8f61f5e7f4092d
갈미현 <kalmihyeon@galmihyeon-ui-MacBookAir.local>
2023-10-04 16:57:39 +0900

    squash commit

--- src/App.js
+++ src/App.js
@@ -82,7 +82,10 @@ function App() {
       <p>stash fix/app_text test</p>
       <p>stash fix/app_text test2</p>
       <p>stash fix/app_text test3</p>
-      <p>stash fix/app_text test44</p>
+      <p>stash fix/app_text test4</p>
+      <p>stash fix/app_text test5</p>
+      <p>stash fix/app_text test6</p>
+      <p>stash fix/app_text test7</p>
       </header>
     </div>
   );
